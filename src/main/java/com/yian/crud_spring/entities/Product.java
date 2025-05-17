cebbd65975bbc6abf653dd9fbc0b2bbd0aecea92
fjg105cdy <146270786+fjg105cdy@users.noreply.github.com>
2025-05-17 15:38:59 +0100

    add table (products)

---
+++ src/main/java/com/yian/crud_spring/entities/Product.java
@@ -0,0 +1,33 @@
+package com.yian.crud_spring.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@SuperBuilder
+@Table(name="PRODUCTS")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "DESCRIPTION", nullable = false)
+    private String description;
+
+    @Column(name = "PRICE", nullable = false)
+    private Long price;
+
+    @Column(name = "PRODUCTIMG")
+    private String productImg;
+
+}
